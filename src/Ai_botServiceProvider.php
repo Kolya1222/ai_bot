11397be867f40290ebcd5527297c0533738c4552
Kolya1222 <belov.belov-ik@yandex.ru>
2025-12-16 16:48:50 +0700

    add_config_inpublish

--- src/Ai_botServiceProvider.php
+++ src/Ai_botServiceProvider.php
@@ -7,32 +7,45 @@ use kolya2320\Ai_bot\Console\Commands\RunAI;
 class Ai_botServiceProvider extends ServiceProvider
 {
     protected $namespace = 'ai_bot';
+    
     public function register()
     {
         $this->commands([
             RunAI::class,
         ]);
+        
         $this->loadPluginsFrom(
             dirname(__DIR__) . '/plugins/'
         );
-        $this->mergeConfigFrom(
-            dirname(__DIR__) .'/config/ai_bot.php', 'services.yandex_cloud'
-        );
+        
         $this->publishes([
-            __DIR__ . '/../publishable/assets'  => MODX_BASE_PATH . 'assets',
+            __DIR__ . '/../publishable/assets' => MODX_BASE_PATH . 'assets',
+            __DIR__ . '/../config/ai_bot.php' => MODX_BASE_PATH . 'assets/plugins/BotAI/ai_bot.php',
         ]);
+        
         $this->app->registerRoutingModule(
             'BotAI module',
             __DIR__ . '/../routes_module.php'
         );
     }
+    
     public function boot()
     {
         $this->mapWebRoutes();
         $this->loadViewsFrom(__DIR__ . '/../views', $this->namespace);
+        $configPath = MODX_BASE_PATH . 'assets/plugins/BotAI/ai_bot.php';
+        
+        if (file_exists($configPath)) {
+            $this->mergeConfigFrom($configPath, 'services.yandex_cloud');
+        } else {
+            $this->mergeConfigFrom(
+                __DIR__ . '/../config/ai_bot.php', 'services.yandex_cloud'
+            );
+        }
     }
+    
     protected function mapWebRoutes(): void
     {
-        $this->loadRoutesFrom( __DIR__ .'/../routes.php'); // Добавляем ваши маршруты
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
     }
 }
